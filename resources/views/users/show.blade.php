515c6795222473b96383c9d92b524168b75306d5
HarlanLuo <luoxwen@gmail.com>
2017-12-02 21:57:08 +0800

    Basic user profile editing

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -14,10 +14,10 @@
                         <div class="media-body">
                             <hr>
                             <h4><strong>个人简介</strong></h4>
-                            <p>This is a person</p>
+                            <p>{{ $user->introduction }}</p>
                             <hr>
                             <h4><strong>注册于</strong></h4>
-                            <p>2016.01.23</p>
+                            <p>{{ $user->created_at->diffForHumans() }}</p>
                         </div>
                     </div>
                 </div>
