89fbe4d58ef288479b60507ff60becf340008ff3
HarlanLuo <luoxwen@gmail.com>
2017-12-02 23:24:53 +0800

    Only authorized user can edit user profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function isAuthor($obj)
+    {
+        return $this->id == $obj->user_id;
+    }
 }
